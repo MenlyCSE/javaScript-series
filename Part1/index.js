3db62e6de80754c17d5f5871f392dfe88360a439
MenlyCSE <MenlyCSE@gmail.com>
2024-08-11 21:27:34 -0400

    update

--- Part1/index.js
+++ Part1/index.js
@@ -1,16 +1,1 @@
-// swapping these values
-let a = 'red';
-let b = 'blue';
-
-// solution start
-let c = a;
-let d = b;
-
-a = d;
-b = c;
-// end
-
-
-// do not change order
-console.log(a);
-console.log(b);
+// loops

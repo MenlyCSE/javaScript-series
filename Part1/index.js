d55fd3e38f5a22e9de69d8f0040e329050254b8c
MenlyCSE <MenlyCSE@gmail.com>
2024-08-13 19:56:30 -0400

    refactoring

--- Part1/index.js
+++ Part1/index.js
@@ -1,16 +1,16 @@
+const isPrime = (number) => {
+    for (let factor = 2; factor < number; factor++) 
+        if (number % factor === 0) 
+            return false;
+
+    return true;
+};
+
 const showPrimes = (limit) => {
     for (let number = 2; number <= limit; number++) {
-
-        let isPrime = true;
-        for (let factor = 2; factor < number; factor++) {
-            if (number % factor === 0) {
-                isPrime = false;
-                break;
-            }
-        }
-
-        if (isPrime) console.log(number);
+        if (isPrime(number))
+            console.log(number);
     }
 };
 
-showPrimes(10); 
+showPrimes(10);  

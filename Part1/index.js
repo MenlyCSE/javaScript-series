e8845b5d3d10ae8809607028b55437f933e5fc1b
MenlyCSE <MenlyCSE@gmail.com>
2024-08-13 07:58:43 -0400

    refactor

--- Part1/index.js
+++ Part1/index.js
@@ -6,18 +6,13 @@ const sum = (limit) => {
     if (typeof limit !== 'number')
         console.log('Please enter a numerical value...');
 
-    let divisibles = {
-        three: 0,
-        five: 0
-    };
-
-    // get multiples
+    let sum = 0;
     for (let i = 0; i <= limit; i++) {
-        (i % 3 === 0) ? divisibles.three += i : divisibles.three += 0;
-        (i % 5 === 0) ? divisibles.five += i : divisibles.five += 0;
+        if ((i % 3 === 0) || (i % 5 === 0))
+            sum += i;
     }
 
-   return `Sum: ${divisibles.three + divisibles.five}`;
+   return `Sum: ${sum}`;
 };
 
 // console.log(sum(10));

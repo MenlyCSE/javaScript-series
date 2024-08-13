b8c5cc2cf40167b36fa3ee2b35c24ca61a330cab
MenlyCSE <MenlyCSE@gmail.com>
2024-08-13 07:55:39 -0400

    sum of multiples exercise

--- Part1/index.js
+++ Part1/index.js
@@ -1,15 +1,23 @@
-// display properties
-const showProperties = (object) => {
-    for (let props in object) 
-        if (typeof object[props] === 'string')
-            console.log(`${props}: ${object[props]}`);
+// 10: 3, 6, 9
+// 10: 5, 10
+// sum: 33
+
+const sum = (limit) => {
+    if (typeof limit !== 'number')
+        console.log('Please enter a numerical value...');
+
+    let divisibles = {
+        three: 0,
+        five: 0
+    };
+
+    // get multiples
+    for (let i = 0; i <= limit; i++) {
+        (i % 3 === 0) ? divisibles.three += i : divisibles.three += 0;
+        (i % 5 === 0) ? divisibles.five += i : divisibles.five += 0;
+    }
+
+   return `Sum: ${divisibles.three + divisibles.five}`;
 };
 
-const movie = {
-    title: 'Aqua man',
-    releaseYear: 2018,
-    rating: 4.5,
-    directors: 'Tony Starx'
-};
-
-showProperties(movie);
+// console.log(sum(10));

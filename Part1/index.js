e3601055060fd4c221cecb4df4f6c211b10e8df6
MenlyCSE <MenlyCSE@gmail.com>
2024-08-15 14:07:21 -0400

    update

--- Part1/index.js
+++ Part1/index.js
@@ -1,17 +1,21 @@
-// OOP
-
-// factory function
-function createCircle(radius) {
-    return {
-        radius,
-        draw() {
-            console.log('mkay');
-        }
-    };
-}
-
-const circle1 = createCircle(1);
-console.log(circle1);
-
-const circle2 = createCircle(2);
-console.log(circle2)
+// objects
+const book = [
+    {
+        title: 'a',
+        summary: 'b',
+        price: '7.99',
+        author: 'c'
+    },
+    {
+        title: 'a',
+        summary: 'b',
+        price: '7.99',
+        author: 'c'
+    },
+    {
+        title: 'a',
+        summary: 'b',
+        price: '7.99',
+        author: 'c'
+    },
+];

1e69e603ab9cba3d5710c87ae38adeed4ebe1f2e
MenlyCSE <MenlyCSE@gmail.com>
2024-08-19 13:00:25 -0400

    reduce method

--- Part1/index.js
+++ Part1/index.js
@@ -1,15 +1,17 @@
 //
 const numbers = [1, 2, 3, 4];
 
-// [1, 2, 3, 4]
-// ----> move(numbers, 0, 1) 
-// [1, 2, 3, 4] ----> [2, 1, 3, 4]
+
+const getMax = array => {
+    // let currentNumber = array[0];
+    // for (let i = 0; i <= array.length; i++)
+    //     if (currentNumber < array[i])
+    //         currentNumber = array[i];
+
+    // return currentNumber;
+
+    return array.reduce((accumulator, current) => (accumulator > current) ? accumulator : current);
+};
 
 
-// [1, 2, 3, 4]
-// ----> move(numbers, 1, 1) 
-// [1, 2, 3, 4] ----> [1, 3, 2, 4]
-
-const move = (array, index, offset) => {
-     
-}
+console.log(getMax(numbers));

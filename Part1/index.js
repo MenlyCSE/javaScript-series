56613ad513ee9da360c22b73bc387545ef6e8425
MenlyCSE <MenlyCSE@gmail.com>
2024-08-19 14:52:31 -0400

    var keyword

--- Part1/index.js
+++ Part1/index.js
@@ -1,32 +1,8 @@
-//
-const movie = [
-    {
-        title: 'Gotham',
-        year: 2018,
-        rating: 4.5
-    },
-    {
-        title: 'Mike Scores',
-        year: 2018,
-        rating: 4.7
-    },
-    {
-        title: 'City Girls',
-        year: 2018,
-        rating: 3
-    },
-    {
-        title: 'City Girls',
-        year: 2017,
-        rating: 4.5
-    },
-];
+// Scope
+{
+    var message = 'Hi';
+}
 
-const result = movie
-    .filter(movie => movie.year === 2018 && movie.rating >= 4)
-    .sort((a, b) => a.rating - b.rating)
-    .reverse()
-    .map(movie => movie.title);
+// var => function scope
 
-
-console.log(result);
+console.log(message);

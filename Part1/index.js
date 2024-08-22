7f4fd33e1a857387a910a7e04fa5599c3b2d219d
MenlyCSE <MenlyCSE@gmail.com>
2024-08-21 23:21:05 -0400

    Update index.js

--- Part1/index.js
+++ Part1/index.js
@@ -1,16 +1,1 @@
-// 
-const numbers = [1, 2, 3];
-
-const testArray = (array) => {
-    if (!Array.isArray(array))
-        throw new Error('Invalid input!');
-
-    console.log('Working :)')
-};
-
-try {
-    console.log(testArray(numbers));
-}
-catch (error) {
-    console.error('NO!');
-}
+//

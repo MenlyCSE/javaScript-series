9886c242f8977d289355df8add615d6eb3106389
MenlyCSE <MenlyCSE@gmail.com>
2024-08-30 13:59:49 -0400

    Update index.js

--- Part2/index.js
+++ Part2/index.js
@@ -1,45 +1,11 @@
-// property: duration - user input
-// method: start: it adds seconds to duration - toggle state
-// method: stop: it stops adding the seconds - toggle state
-// method: reset: it resets the duration property
+// prototype = parent :)
+let person = { name: 'Mosh' };
 
-function Stopwatch() {
-    let startTime = null;
-    let endTime = null;
-    let running = false;
-    let duration = 0;
+Object.defineProperty(person, 'name', {
+    writable: false,
+    enumerable: true
+});
 
-    // functions
-    this.start = function() {
-        if (running) 
-            throw new Error('Stop watch already running!!');
+person.name = 'john';
 
-        running = true;
-        startTime = new Date();
-    }
-
-    this.stop = function() {
-        if (!running)
-            throw new Error('Stopwatch has not started...');
-
-        running = false;
-        endTime = new Date();
-        duration += (endTime.getTime() - startTime.getTime()) / 1000;
-    }
-
-    this.reset = function() {
-        startTime = null;
-        endTime = null;
-        running = false;
-        duration = 0;
-    }
-
-    // getter
-    Object.defineProperty(this, 'duration', {
-        get: function() {
-            return duration;
-        }
-    });
-}
-
-const sw = new Stopwatch();
+console.log(Object.keys(person));

e72ef84f54171c33dcd119827aa0b8f7a269945b
MenlyCSE <MenlyCSE@gmail.com>
2024-09-04 18:02:57 -0400

    premature optimization

--- Part2/index.js
+++ Part2/index.js
@@ -1,11 +1,62 @@
-// prototype = parent :)
-let person = { name: 'Mosh' };
+function Stopwatch() {
+  let startTime = null;
+  let endTime = null;
+  let running = false;
+  let duration = 0;
 
-Object.defineProperty(person, 'name', {
-    writable: false,
-    enumerable: true
-});
+  // getter
+  Object.defineProperty(this, 'duration', {
+    get: function () {
+      return duration;
+    },
+    set: function (value) {
+      duration = value;
+    }
+  });
 
-person.name = 'john';
+  Object.defineProperty(this, 'startTime', {
+    get: function () {
+      return startTime;
+    }
+  });
 
-console.log(Object.keys(person));
+  Object.defineProperty(this, 'endTime', {
+    get: function () {
+      return endTime;
+    }
+  });
+
+  Object.defineProperty(this, 'running', {
+    get: function () {
+      return running;
+    }
+  });
+}
+
+Stopwatch.prototype.start = function () {
+  if (this.running)
+    throw new Error('Stop watch already running!!');
+
+  this.running = true;
+  this.startTime = new Date();
+};
+
+Stopwatch.prototype.stop = function () {
+  if (!this.running)
+    throw new Error('Stopwatch has not started...');
+
+  this.running = false;
+  this.endTime = new Date();
+  this.duration += (endTime.getTime() - startTime.getTime()) / 1000;
+};
+
+Stopwatch.prototype.reset = function () {
+  this.startTime = null;
+  this.endTime = null;
+  this.running = false;
+  this.duration = 0;
+};
+
+
+const sw = new Stopwatch();
+console.log(sw);

24bd22aae7a5113232639313db6947a3c850a32e
MenlyCSE <MenlyCSE@gmail.com>
2024-09-10 15:16:37 -0400

    Update index.js

--- Part2/index.js
+++ Part2/index.js
@@ -1,4 +1,15 @@
-
 function HtmlElement() {
-
+    this.click = function() {
+        console.log('clicked');
+    }
 }
+
+HtmlElement.prototype.focus = function () {
+    console.log('Focused');
+}
+
+function HtmlSelectElement(items, addItem, removeItem) {
+    
+}
+
+HtmlSelectElement.prototype = Object.create(HtmlElement.prototype);

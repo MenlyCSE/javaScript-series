37f4ed487621d080959f6c9a68a175d5e6b698cc
MenlyCSE <MenlyCSE@gmail.com>
2024-09-14 18:24:23 -0400

    Update index.js

--- Part2/index.js
+++ Part2/index.js
@@ -1,27 +1,7 @@
-// function Circle(radius) {
-//     this.radius = radius;
-
-//     this.draw = function() {
-//         console.log('draw');
-//     }
-// }
-
-class Circle {
-    constructor(radius) {
-        this.radius = radius;
-        this.move = function() {}
-    }
-
-    // instance
-    draw() {
-        console.log('draw');
-    }
-
-    // Static
-    static parse(str) {
-        const radius = JSON.parse(str).radius;
-        return new Circle(radius);
+class Math2 {
+    static abs(value) {
+        // magic
     }
 }
 
-const c = new Circle(1);
+Math2.abs()

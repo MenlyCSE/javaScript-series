4208fb1a6026435b96d239ad263d283bd5b0cff3
MenlyCSE <MenlyCSE@gmail.com>
2024-09-14 14:32:18 -0400

    modern classes in ES6

---
+++ Part2/index.js
@@ -0,0 +1,18 @@
+// function Circle(radius) {
+//     this.radius = radius;
+
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+        this.move = function() {}
+    }
+
+    draw() {
+        console.log('draw');
+    }
+}
